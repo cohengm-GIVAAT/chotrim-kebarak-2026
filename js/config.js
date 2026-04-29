c480e4500c4e17efb9938c1a1df84bf28c5af0b1
cohengm-GIVAAT <cohengm@gmail.com>
2026-04-29 09:05:09 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
 const CONFIG = {
 
   // ► שנה לכתובת ה-Apps Script Web App שלך (ראה SETUP.md)
-SHEET_URL: "https://script.google.com/macros/s/AKfycbxcWAY_PIujH07vZRW0b9JRsIv5cI7Nmml8hCynV25tROpaYmosjkXyrpkIwOzi3LOU/exec",
+SHEET_URL: "https://script.google.com/macros/s/AKfycbyCSaq7xz5MqKb07kZHYMk9ysMfDelMgulbV7rjg9ZYyPqHZ7S8Ubyf62soTtUy-ANx/exec",
   
   // ► קישור ל-PayBox האישי שלך
   PAYBOX_URL: "https://www.payboxapp.com/",
